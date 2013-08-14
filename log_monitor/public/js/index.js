7e4cdc81fff24837830b2141c50de9de7701a021
TimeExceed <tyf00@aliyun.com>
2013-08-14 09:38:28 +0800

    discard requestChart

--- log_monitor/public/js/index.js
+++ log_monitor/public/js/index.js
@@ -57,44 +57,9 @@
 				$('#divContentTable').show();
 			} else {
 				$('#divContentChart').show();
-				// requestChart();
                 log_monitor.core.draw_request_chart();
 			}
 		}
-
-		var requestChart = function() {
-			var data = {
-				'data1' : {'test1':10,'test2':12,'test3':15,'test4':5,'test5':12,'test6':12,'test7':2,'test8':9},
-				'data2' : {'test1':5,'test2':11,'test3':10,'test4':11,'test5':13,'test6':10,'test7':21,'test8':19},
-				'data3' : {'test1':2,'test2':15,'test3':6,'test4':9,'test5':23,'test6':5,'test7':17,'test8':7},
-			};
-			var categories = ['test1','test2','test3','test4','test5','test6','test7','test8'];
-			var config = ChartConfig.getConfig('area');
-			var dataTmp = [],dataValue;
-			var finData = [];
-			for (var x in data) {
-				var dataSeries = new Array;
-				for(var index in data[x]) {
-					dataSeries.push(Number(data[x][index]));
-				}
-				finData.push({name:x,data:dataSeries});
-			}
-			//config.legend = false;
-			config.title.text = '';
-			config.series = finData;
-			config.xAxis.categories = categories;
-			//config.xAxis.min = 1;
-			config.xAxis.title.text = '';
-			//config.xAxis.type = 'logarithmic';
-			//config.yAxis.min = 1;
-			//config.yAxis.title.text = '';
-			config.tooltip.headerFormat = '';
-			config.tooltip.formatter = function() {
-				return (this.point.category) + '总数' + this.point.y+'次';
-			};
-			//config.plotOptions.column.pointWidth = 15;
-			$("#divContentChart").highcharts(config);
-		};
 
 		buttonSwitcher()
         log_monitor.core.refresh();
